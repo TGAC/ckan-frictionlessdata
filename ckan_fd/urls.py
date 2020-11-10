84c8f70372e1f7d833511afa438fae44a26db7dc
Xingdong Bian <bianxingdong@gmail.com>
2020-11-10 11:17:25 +0000

    fix search view

--- ckan_fd/urls.py
+++ ckan_fd/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('list_all/', views.list_all),
-    path('search', views.search_ckan),
+    path('search', views.ckan_search),
 ]
